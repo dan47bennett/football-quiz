a8075eea1fb69ce42a845566cf18fc1130827c2d
Dan Bennett <dghbennett47@gmail.com>
2026-06-30 22:27:14 +0100

    feat: scrape team data from site

--- server/index.ts
+++ server/index.ts
@@ -1,38 +1,54 @@
 import { Builder, Browser, By, WebDriver } from 'selenium-webdriver';
 
-async function example() {
+async function scrape() {
     console.log('Starting...');
 
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
-    console.log('driver: ', driver);
     try {
         await driver.get(
             'https://www.premierleague.com/en/tables/premier-league/1993-94/',
         );
-        const maybeAcceptButton = await driver.findElement(
-            By.id('onetrust-accept-btn-handler'),
-        );
 
-        if (maybeAcceptButton) {
-            maybeAcceptButton.click();
+        const teams: Record<string, string>[] = [];
+
+        for (let i = 1; i < 25; i++) {
+            const xpath = `//*[@id="main-content"]/div[1]/div[2]/section/div[2]/table/tbody/tr[${i}]`;
+
+            try {
+                const element = await driver.findElement(By.xpath(xpath));
+
+                if (element) {
+                    const columns = await element.findElements(
+                        By.tagName('td'),
+                    );
+
+                    const teamName = await columns[1].getText();
+                    const goalsFor = await columns[6].getText();
+                    const goalsAgainst = await columns[7].getText();
+                    const goalDifference = await columns[8].getText();
+
+                    const teamValue = {
+                        teamName,
+                        goalDifference,
+                        goalsAgainst,
+                        goalsFor,
+                    };
+
+                    teams.push(teamValue);
+                }
+            } catch (err) {
+                console.log('err: ', err);
+            }
         }
 
-        const found = await driver.findElement(
-            By.xpath(
-                '//*[@id="main-content"]/div[1]/div[2]/section/div[2]/table/tbody/tr[1]/td[2]/div[2]/span[1]',
-            ),
-        );
-
-        const text = await found.getText();
-        console.log('text: ', text);
+        console.log('teams: ', teams);
     } finally {
         await driver.quit();
     }
 }
 
 async function main() {
-    await example();
+    await scrape();
 }
-console.log('Starting...');
 
 void main();
